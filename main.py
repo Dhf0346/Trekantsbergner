6ab02db036b9c9785e517de0cb82c25c1edd433a
dhf03 <dhf0346@gmail.com>
2024-03-07 10:21:17 +0100

    New one

--- main.py
+++ main.py
@@ -4,38 +4,42 @@ print("B: Du kender 2 sider og 1 vinkel.")
 print("C: Du kender 1 side og 2 vinkler.")
 valg=(input("indsæt det du ville bruge:")).lower()
 
-# Kører programet hvis du vælger A eller a
-if 'a' in valg:
-    print("Du valgte A")
-    side1 = float(input("Hvor lang er den første side?"))
-    side2 = float(input("Hvor lang er den anden side?"))
-    side3 = float(input("Hvor lang er den sidste side?"))
-
-elif 'b' in valg:
-    print("Du valgte B")
-    while True:
+while True:
+# Kører programmet hvis du vælger a eller A.
+    if 'a' in valg:
+        print("Du valgte A")
         side1 = float(input("Hvor lang er den første side?"))
         side2 = float(input("Hvor lang er den anden side?"))
-        vinkel1 = float(input("Hvad er vinklen?"))
-        if vinkel1<180:
-            print("Din vinkel er valid.")
-            break
-        else:
-            print("Øv, din vinkel er ikke valid, prøv igen:")
+        side3 = float(input("Hvor lang er den sidste side?"))
 
-elif 'c' in valg:
-    print("Du valgte C")
-    while True:
-        side1 = float(input("Hvor lang er den første side?"))
-        vinkel1= float(input("Hvad er den første vinkel?"))
-        vinkel2 = float(input("Hvad er den anden vinkel?"))
-        if vinkel1 + vinkel2<180:
-            print("Din vinkeler er valid.")
-            break
-        else:
-            print("Øv, din vinkler er ikke valid, prøv igen:")
+    # Kører programmet hvis du vælger b eller B.
+    elif 'b' in valg:
+        print("Du valgte B")
+        b_vara = True
+        while b_vara is True:
+            side1 = float(input("Hvor lang er den første side?"))
+            side2 = float(input("Hvor lang er den anden side?"))
+            vinkel1 = float(input("Hvad er vinklen?"))
+            if vinkel1<180:
+                print("Din vinkel er valid.")
+                b_vara = False
+            else:
+                print("Øv, din vinkel er ikke valid, prøv igen:")
 
-else:
-    while True:
+    # Kører progemmet hvis du vægler c eller C.
+    elif 'c' in valg:
+        print("Du valgte C")
+        c_vara = True
+        while c_vara is True:
+            side1 = float(input("Hvor lang er den første side?"))
+            vinkel1= float(input("Hvad er den første vinkel?"))
+            vinkel2 = float(input("Hvad er den anden vinkel?"))
+            if vinkel1 + vinkel2<180:
+                print("Din vinkeler er valid.")
+                c_vara = False
+            else:
+                print("Øv, din vinkler er ikke valid, prøv igen:")
+
+    else:
         print("Du har indtastet noget ugyldig, prøv igen")
-        break
+    break
